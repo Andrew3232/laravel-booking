6316643779d13ea4cabd0823591d28cf19c05810
Andrew3232 <oleinitch@gmail.com>
2023-08-08 07:36:43 +0300

    set web routes

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::get('{any}', function () {
+    return view('welcome');
+})->where('any', '.*');
 
 Auth::routes();
 
